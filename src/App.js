354f12b8e42530f31a37113e097979c139355d1a
VICTOR EDUARDO PEIXER MUNARIM <victor_munarim@estudante.sc.senai.br>
2025-04-07 16:34:02 -0300

    tarefa existente

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import TaskList from '../src/componentes/TaskList';
 import { useState } from 'react';
 
 function App() {
-
-  const [tarefasTemporario, setTarefasTemporario] = useState(null)
-  const [tarefas, setTarefas] = useState([])
+  const [tarefasTemporario, setTarefasTemporario] = useState(null);
+  const [tarefas, setTarefas] = useState([]);
 
   function adicionarTarefa(event) {
-    setTarefasTemporario(event.target.value)
+    setTarefasTemporario(event.target.value);
   }
 
   function enviar() {
-    setTarefas([...tarefas, { task: tarefasTemporario, existe: true, feito: false }])
-    setTarefasTemporario(null)
+    // Verificar se a tarefa já existe na lista
+    const tarefaExistente = tarefas.some(tarefa => tarefa.task === tarefasTemporario);
+
+    if (!tarefaExistente && tarefasTemporario) {
+      setTarefas([...tarefas, { task: tarefasTemporario, existe: true, feito: false }]);
+    } else {
+      alert('Tarefa já existe');
+    }
+    setTarefasTemporario(null);
   }
 
   return (
@@ -25,9 +31,14 @@ function App() {
           type='text'
           placeholder='Escreva a tarefa'
           onChange={adicionarTarefa}
+          value={tarefasTemporario || ''}
           required
         ></input>
-        <input className='botao' type='submit' onClick={tarefasTemporario != null ? enviar : null}></input>
+        <input
+          className='botao'
+          type='submit'
+          onClick={tarefasTemporario != null ? enviar : null}
+        ></input>
       </div>
       <div className='saida'>
         <TaskList tarefas={tarefas} />
